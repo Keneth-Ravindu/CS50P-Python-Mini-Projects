3853b4a2cbac39944d6a3083654d06f4bc7640e1
Keneth Ravindu <kenethravindu2000@gmail.com>
2026-09-10 17:35:53 +0530

    Update expense_tracker.py

--- Lesson2/expense_tracker.py
+++ Lesson2/expense_tracker.py
@@ -5,21 +5,26 @@ def main():
 
 
 def expense_tracker():
-    total = 0
+    expenses = []
     while True:
         print("\n===== EXPENSE TRACKER =====")
         print("1. Add expense")
-        print("2. View total")
-        print("3. Exit")   
+        print("2. View all expenses")
+        print("3. View total")
+        print("4. Exit")   
         choice = int(input("Choose an option: "))
         
         if choice == 1:
-            total += add_expense()
+            expense = add_expense()
+            expenses.append(expense)
             
         elif choice == 2:
-            display_total(total)
+            display_expenses(expenses)
             
         elif choice == 3:
+            display_total(expenses)
+            
+        elif choice == 4:
             print("Have a nice day!!!")
             break
         else:
@@ -30,8 +35,16 @@ def add_expense():
     return expense
 
 
-def display_total(total):
-    print(f"Total = {total}")
+def display_total(expenses):
+    total = 0
     
+    for expense in expenses:
+        total += expense
+    
+    print(f"Total = {total}")
+
+def display_expenses(expenses):
+    for i in range(len(expenses)):
+        print(f"{i + 1}. {expenses[i]}")
         
 main()
